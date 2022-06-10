65af178ab5e4aa8b7c3e9f72407231365bcc8de9
alessiospb <lelik.guru@gmail.com>
2022-06-10 10:56:24 +0300

    From main to login page

--- test_main_page.py
+++ test_main_page.py
@@ -3,8 +3,10 @@ from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.should_be_login_link()
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
